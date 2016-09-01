87daf2b671b4b00b87f76b351fd07897a52da805
D'sardy <shubhamsardar07@gmail.com>
2016-09-01 16:29:39 +0530

    Initial commit. home page fixed

---
+++ app/src/main/java/rtxindia/com/rtxapp/homeFragment.java
@@ -0,0 +1,146 @@
+package rtxindia.com.rtxapp;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ViewAnimator;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link homeFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link homeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class homeFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    LinearLayout dashboard;
+
+    TextView tvco,tvuco,tvdpo,tvcco;
+
+    private OnFragmentInteractionListener mListener;
+
+    public homeFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment homeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static homeFragment newInstance(String param1, String param2) {
+        homeFragment fragment = new homeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=  inflater.inflate(R.layout.fragment_home, container, false);
+
+        //ger ref
+
+        tvcco = (TextView)view.findViewById(R.id.textViewcco);
+        tvdpo = (TextView)view.findViewById(R.id.textViewdp);
+        tvco = (TextView)view.findViewById(R.id.textViewco);
+        tvuco = (TextView)view.findViewById(R.id.textViewuco);
+        startCountAnimation();
+
+
+
+
+
+        return view;
+    }
+    private void startCountAnimation() {
+        ValueAnimator animator = new ValueAnimator();
+        animator.setObjectValues(0, 61);
+        animator.setDuration(3000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator animation) {
+                tvcco.setText("" + (int) animation.getAnimatedValue());
+            }
+        });
+        animator.start();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
