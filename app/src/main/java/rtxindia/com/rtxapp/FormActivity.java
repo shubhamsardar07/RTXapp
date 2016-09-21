810d7b4a93b27ea81867dba215b34cb8cc43177c
D'sardy <shubhamsardar07@gmail.com>
2016-09-21 21:15:03 +0530

    form , UI comp1

--- app/src/main/java/rtxindia/com/rtxapp/FormActivity.java
+++ app/src/main/java/rtxindia/com/rtxapp/FormActivity.java
@@ -1,18 +1,30 @@
 package rtxindia.com.rtxapp;
 
 import android.app.ActionBar;
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class FormActivity extends AppCompatActivity {
 
     Spinner spinner,spinner2,spinner3;
     NumberPicker nMax,nMin;
+    DatePickerDialog datePickerDialog;
+    SimpleDateFormat dateFormatter;
+    EditText startDateEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +35,32 @@ public class FormActivity extends AppCompatActivity {
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        Calendar newCalendar = Calendar.getInstance();
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                startDateEditText.setText(dateFormatter.format(newDate.getTime()));
+            }
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+
+
         spinner = (Spinner)findViewById(R.id.spinner);
         spinner2 = (Spinner)findViewById(R.id.spinner2);
         spinner3 = (Spinner)findViewById(R.id.spinner3);
+        startDateEditText = (EditText)findViewById(R.id.editText4);
+        startDateEditText.setInputType(InputType.TYPE_NULL);
+        startDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                datePickerDialog.show();
+
+            }
+        });
 
         nMax = (NumberPicker)findViewById(R.id.numberPicker1);
         nMax.setMinValue(0);
---
+++ app/src/main/java/rtxindia/com/rtxapp/UpcomingOrders.java
@@ -0,0 +1,13 @@
+package rtxindia.com.rtxapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpcomingOrders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upcoming_orders);
+    }
+}
